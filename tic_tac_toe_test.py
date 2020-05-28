1acc3b952026c41792beb6b206b27f12d5c465c1
Leo Nistor <leo.nistor@gmail.com>
2020-05-28 17:19:55 +0300

    more tests

--- tic_tac_toe_test.py
+++ tic_tac_toe_test.py
@@ -28,6 +28,27 @@ def test_moves():
     game.move(8)
     assert game.board == ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
 
-# TODO
-# winner tests
-# tie tests
+
+def test_x_winner():
+    game = Tic_tac_toe()
+    for m in [0, 1, 4, 2, 8]:
+        game.move(m)
+    assert game.get_winner() == 'X'
+    game = Tic_tac_toe()
+    for m in range(7):
+        game.move(m)
+    assert game.get_winner() == 'X'
+
+
+def test_o_winner():
+    game = Tic_tac_toe()
+    for m in [0, 4, 1, 3, 8, 5]:
+        game.move(m)
+    assert game.get_winner() == 'O'
+
+
+def test_tie():
+    game = Tic_tac_toe()
+    for m in [0, 2, 1, 3, 5, 4, 6, 7, 8]:
+        game.move(m)
+    assert game.get_winner() == 'TIE'
