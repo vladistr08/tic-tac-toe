096d36511de9c99cbbae7c3d34ba08c684b8ecbc
Leo Nistor <leo.nistor@gmail.com>
2020-06-04 17:17:16 +0300

    colored index

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,19 +1,23 @@
 from terminaltables import SingleTable
 from fabulous.color import bold, bg256, fg256
-#the tic-tac-toe class
+from fabulous.color import blue
+# import numpy as np
+# the tic-tac-toe class
+
+
 class Tic_tac_toe:
     # useful game atributes
     player_curent = ""
     board = []
     message = ""
 
-    #the construnctor of the class
-    def __init__(self, player_curent = "X"):
+    # the construnctor of the class
+    def __init__(self, player_curent="X"):
         self.player_curent = player_curent
         # fill the board with ' ' characther
         self.board = [' ' for i in range(9)]
 
-    #the main function where the game starts
+    # the main function where the game starts
     def start_game(self):
         print("This is the Board, the position where you put X or O is from 0-8 from left to right and up to down!")
         self.print_board()
@@ -21,7 +25,7 @@ class Tic_tac_toe:
         if self.get_winner() == 'TIE':
             print("It is a TIE!")
         else:
-            print("The winner is " + self.get_winner() + " congratulations!") 
+            print("The winner is " + self.get_winner() + " congratulations!")
 
     def __line_win(self, sign):
         for i in range(0, 9, 3):
@@ -67,9 +71,9 @@ class Tic_tac_toe:
             return True
         else:
             return False
-            
+
     def user_input(self):
-        intrare =  input(bold("On wich position (0-8) :"))
+        intrare = input(bold("On wich position (0-8) :"))
         if len(intrare) != 1:
             return self.user_input()
         if ord(intrare) >= ord('0') and ord(intrare) <= ord('8'):
@@ -93,21 +97,25 @@ class Tic_tac_toe:
             if self.player_curent == 'X':
                 self.player_curent = 'O'
             else:
-                self.player_curent = 'X' 
+                self.player_curent = 'X'
 
     def print_board(self):
         data = [[self.board[0], self.board[1], self.board[2]],
-               [self.board[3], self.board[4], self.board[5]],
-               [self.board[6], self.board[7], self.board[8]]
-         ]
+                [self.board[3], self.board[4], self.board[5]],
+                [self.board[6], self.board[7], self.board[8]]
+                ]
+
+        display_data = [cell + "\n" + blue(index) for index,
+                        cell in enumerate(self.board)]
+
+        data = [display_data[0:3], display_data[3:6], display_data[6:]]
         table = SingleTable(data)
         table.inner_row_border = True
         print(table.table)
         print()
 
     def print_game(self):
-        print(bg256("blue","Current Player: " + self.player_curent))
+        print(bg256("blue", "Current Player: " + self.player_curent))
         print(bg256("red", self.message))
         self.message = ""
         self.print_board()
-
